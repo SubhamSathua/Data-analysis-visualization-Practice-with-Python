34dade1eb7e665aad87fabcc0b5bc73bc07a0903
Subham Kumar Sathua <137478528+SubhamSathua@users.noreply.github.com>
2025-12-26 11:33:32 +0530

    tuple

---
+++ Basic Programming/Set.py
@@ -0,0 +1,18 @@
+x = {1,2,3,4}
+y = {5,6,7,8, 9, 0}
+z = {7,8}
+
+print(y-x)
+print(y-z)
+diff = z.symmetric_difference(y)
+print(diff) 
+
+
+a = set('abcdef')
+b = set('klmnopqrstuvwxyz')
+
+print(a, b)
+result = a ^ b
+print(sorted(result))
+
+
